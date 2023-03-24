549a23d1a73d4e69b155e41be0bb0a2935c44fcc
DaMeep <jayky752@gmail.com>
2023-03-24 14:20:07 -0400

    Initial Commit

---
+++ app/src/main/java/com/example/wateryouwaitingfor/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.wateryouwaitingfor;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import com.example.wateryouwaitingfor.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment((new HomeFragment()));
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.getMenu().findItem(R.id.home).setChecked(true);
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch(item.getItemId()){
+
+                case R.id.contamination:
+                    replaceFragment(new ContaminationFragment());
+                    break;
+                case R.id.stats:
+                    replaceFragment(new StatsFragment());
+                    break;
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.friends:
+                    replaceFragment(new FriendsFragment());
+                    break;
+                case R.id.settings:
+                    replaceFragment(new SettingsFragment());
+                    break;
+
+            }
+
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
+    }
+}
