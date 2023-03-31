ad77ab939dfe5e0558c29d1c0de5e84e7df1a590
ashwinabangari <abangari@wpi.edu>
2023-03-31 13:45:57 -0400

    3/29/23 Update: SQL Database is in

--- app/src/main/java/com/example/wateryouwaitingfor/DBHandler.java
+++ app/src/main/java/com/example/wateryouwaitingfor/DBHandler.java
@@ -8,29 +8,30 @@ import android.database.sqlite.SQLiteOpenHelper;
 
         // creating a constant variables for our database.
         // below variable is for our database name.
-        private static final String DB_NAME = "coursedb";
+        private static final String DB_NAME = "waterconsumedb";
 
         // below int is our database version
         private static final int DB_VERSION = 1;
 
         // below variable is for our table name.
-        private static final String TABLE_NAME = "mycourses";
+        private static final String TABLE_NAME = "dailywater";
+        // below variable is for our second table name.
+        private static final String SECONDTABLE_NAME = "overalldata";
 
         // below variable is for our id column.
         private static final String ID_COL = "id";
 
-        // below variable is for our course name column
-        private static final String NAME_COL = "name";
+        // below variable is for our time of consumption column
+        private static final String TIME_COL = "time";
 
-        // below variable id for our course duration column.
-        private static final String DURATION_COL = "duration";
+        // below variable is for our amount consumed column.
+        private static final String AMOUNT_COL = "amount";
 
-        // below variable for our course description column.
-        private static final String DESCRIPTION_COL = "description";
+        // below variable is for our date column for table 2.
+        private static final String DATE_COL = "date";
 
-        // below variable is for our course tracks column.
-        private static final String TRACKS_COL = "tracks";
-
+        // below variable is for our daily total column for Table 2.
+        private static final String DAYTOT_COL = "daily totals";
         // creating a constructor for our database handler.
         public DBHandler(Context context) {
             super(context, DB_NAME, null, DB_VERSION);
@@ -45,18 +46,24 @@ import android.database.sqlite.SQLiteOpenHelper;
             // along with their data types.
             String query = "CREATE TABLE " + TABLE_NAME + " ("
                     + ID_COL + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-                    + NAME_COL + " TEXT,"
-                    + DURATION_COL + " TEXT,"
-                    + DESCRIPTION_COL + " TEXT,"
-                    + TRACKS_COL + " TEXT)";
+                    + TIME_COL + " TEXT,"
+                    + AMOUNT_COL + " REAL )";
+
+            String query2 = "CREATE TABLE "+ SECONDTABLE_NAME + " ("
+            + DATE_COL + "TEXT, "
+            + DAYTOT_COL+ "REAL )";
+
+
+
 
             // at last we are calling a exec sql
             // method to execute above sql query
             db.execSQL(query);
+            db.execSQL(query2);
         }
 
-        // this method is use to add new course to our sqlite database.
-        public void addNewCourse(String courseName, String courseDuration, String courseDescription, String courseTracks) {
+        // this method is use to add new drinks to our sqlite database.
+        public void addNewDrink(String time, String Consumed) {
 
             // on below line we are creating a variable for
             // our sqlite database and calling writable method
@@ -69,10 +76,8 @@ import android.database.sqlite.SQLiteOpenHelper;
 
             // on below line we are passing all values
             // along with its key and value pair.
-            values.put(NAME_COL, courseName);
-            values.put(DURATION_COL, courseDuration);
-            values.put(DESCRIPTION_COL, courseDescription);
-            values.put(TRACKS_COL, courseTracks);
+            values.put(TIME_COL, time);
+            values.put(AMOUNT_COL, Consumed);
 
             // after adding all values we are passing
             // content values to our table.
@@ -80,9 +85,36 @@ import android.database.sqlite.SQLiteOpenHelper;
 
             // at last we are closing our
             // database after adding database.
-            db.close();
+
+            addNewDayTot(time, Consumed);
         }
 
+        // this method is use to add new drinks to our sqlite database.
+        public void addNewDayTot(String date, String Totals) {
+
+            // on below line we are creating a variable for
+            // our sqlite database and calling writable method
+            // as we are writing data in our database.
+            SQLiteDatabase datab = this.getWritableDatabase();
+
+            // on below line we are creating a
+            // variable for content values.
+            ContentValues values2 = new ContentValues();
+
+            // on below line we are passing all values
+            // along with its key and value pair.
+            values2.put(DATE_COL, date);
+            values2.put(DAYTOT_COL, Totals);
+
+            // after adding all values we are passing
+            // content values to our table.
+            datab.insert(SECONDTABLE_NAME, null, values2);
+
+
+            // at last we are closing our
+            // database after adding database.
+
+        }
         @Override
         public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
             // this method is called to check if the table exists already.
--- app/src/main/java/com/example/wateryouwaitingfor/MainActivity.java
+++ app/src/main/java/com/example/wateryouwaitingfor/MainActivity.java
@@ -17,9 +17,8 @@ public class MainActivity extends AppCompatActivity {
 
 
         // creating variables for our edittext, button and dbhandler
-        private EditText courseNameEdt, courseTracksEdt, courseDurationEdt, courseDescriptionEdt;
-        private Button addCourseBtn;
-        private DBHandler dbHandler;
+        private EditText timeEdt, amtConsumeEdt;
+    private DBHandler dbHandler;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
@@ -57,43 +56,39 @@ public class MainActivity extends AppCompatActivity {
             });
 
             // initializing all our variables.
-            courseNameEdt = findViewById(R.id.idEdtCourseName);
-            courseTracksEdt = findViewById(R.id.idEdtCourseTracks);
-            courseDurationEdt = findViewById(R.id.idEdtCourseDuration);
-            courseDescriptionEdt = findViewById(R.id.idEdtCourseDescription);
-            addCourseBtn = findViewById(R.id.idBtnAddCourse);
+            timeEdt = findViewById(R.id.idEdtTime);
+            amtConsumeEdt = findViewById(R.id.idEdtAmtConsumed);
+            Button addWaterBtn = findViewById(R.id.idBtnAddWater);
 
             // creating a new dbhandler class
             // and passing our context to it.
             dbHandler = new DBHandler(MainActivity.this);
 
-            // below line is to add on click listener for our add course button.
-            addCourseBtn.setOnClickListener(new View.OnClickListener() {
+            // below line is to add on click listener for our add drink button.
+            addWaterBtn.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
 
                     // below line is to get data from all edit text fields.
-                    String courseName = courseNameEdt.getText().toString();
-                    String courseTracks = courseTracksEdt.getText().toString();
-                    String courseDuration = courseDurationEdt.getText().toString();
-                    String courseDescription = courseDescriptionEdt.getText().toString();
+                    String time = timeEdt.getText().toString();
+                    String Consumed = amtConsumeEdt.getText().toString();
+
 
                     // validating if the text fields are empty or not.
-                    if (courseName.isEmpty() && courseTracks.isEmpty() && courseDuration.isEmpty() && courseDescription.isEmpty()) {
+                    if (time.isEmpty() && Consumed.isEmpty()) {
                         Toast.makeText(MainActivity.this, "Please enter all the data..", Toast.LENGTH_SHORT).show();
                         return;
                     }
 
                     // on below line we are calling a method to add new
                     // course to sqlite data and pass all our values to it.
-                    dbHandler.addNewCourse(courseName, courseDuration, courseDescription, courseTracks);
+                    dbHandler.addNewDrink(time, Consumed);
+                    dbHandler.addNewDayTot(time, Consumed);
 
                     // after adding the data we are displaying a toast message.
-                    Toast.makeText(MainActivity.this, "Course has been added.", Toast.LENGTH_SHORT).show();
-                    courseNameEdt.setText("");
-                    courseDurationEdt.setText("");
-                    courseTracksEdt.setText("");
-                    courseDescriptionEdt.setText("");
+                    Toast.makeText(MainActivity.this, "Drink has been added.", Toast.LENGTH_SHORT).show();
+                    timeEdt.setText("");
+                    amtConsumeEdt.setText("");
                 }
             });
         }
