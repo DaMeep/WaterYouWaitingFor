628943100d2bff5070bf35dccba607044db631b1
Ashwina Bangari <ashbangari@gmail.com>
2023-04-13 11:28:12 -0400

    sql update: taricco fixed stats page (maybe)

--- app/src/main/java/com/example/wateryouwaitingfor/StatsFragment.java
+++ app/src/main/java/com/example/wateryouwaitingfor/StatsFragment.java
@@ -17,7 +17,7 @@ import android.widget.Toast;
  * Use the {@link StatsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class StatsFragment extends Fragment {
+public class StatsFragment extends Fragment implements View.OnClickListener {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -29,11 +29,21 @@ public class StatsFragment extends Fragment {
     private String mParam2;
 
     private String time ;
-    private String Consumed;
+    private String consumed;
 
     public StatsFragment() {
         // Required empty public constructor
     }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment StatsFragment.
+     */
+    // TODO: Rename and change types and number of parameters
     public static StatsFragment newInstance(String param1, String param2) {
         StatsFragment fragment = new StatsFragment();
         Bundle args = new Bundle();
@@ -43,49 +53,64 @@ public class StatsFragment extends Fragment {
         return fragment;
     }
     private Button waterButton;
+    private EditText timeEditText;
+    private EditText consumedEditText;
+
+    private DBHandler db;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d("Ashwina", "inOnCreate" );
+        Log.d("Ashwina", "In StatsFragment: onCreate" );
 
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+        db = new DBHandler(getContext());
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        Log.d("Ashwina", "inOnCreateView");
-        DBHandler db = new DBHandler(getContext());
-        View v = getView();
-            waterButton = (Button)v.findViewById(R.id.btnAddWater);
-            waterButton.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View view) {
-                    Log.d("Ashwina", "inOnClick");
-                    // Inflate the layout for this fragment
-                    // below line is to get data from all edit text fields.
-                    time = ((EditText) view.findViewById(R.id.timeEdt)).getText().toString();
-                    Consumed = ((EditText) view.findViewById(R.id.AmtConsumedEdt)).getText().toString();
+        Log.d("Ashwina", "In StatsFragment: onCreateView" );
+        View myView = inflater.inflate(R.layout.fragment_stats, container, false);
+        waterButton = (Button) myView.findViewById(R.id.btnAddWater);
+        waterButton.setOnClickListener(this);
+        timeEditText = myView.findViewById(R.id.timeEdt);
+        consumedEditText = myView.findViewById(R.id.AmtConsumedEdt);
 
-                    // validating if the text fields are empty or not.
-                    if (time.isEmpty() && Consumed.isEmpty()) {
-                        Toast.makeText(getContext(), "Please enter all the data...", Toast.LENGTH_SHORT).show();
-                        System.out.println(inflater.inflate(R.layout.fragment_stats, container, false));
-                    }
+        return myView;
+    }
 
-                    // on below line we are calling a method to add the new
-                    // drink to sqlite data and pass all our values to it.
-                    db.addNewDrink(time, Consumed);
+    @Override
+    public void onClick(View view) {
+        Log.d("Ashwina", "In StatsFragment: onClick");
 
-                    // after adding the data we are displaying a toast message.
-                    Toast.makeText(getContext(), "Drink has been added.", Toast.LENGTH_SHORT).show();
-                    ((EditText) view.findViewById(R.id.timeEdt)).setText("");
-                    ((EditText) view.findViewById(R.id.AmtConsumedEdt)).setText("");
+        switch (view.getId()) {
+            case R.id.btnAddWater:
+                Log.d("Ashwina", "In StatsFragment: onClick: btnAddWater");
+
+                // below line is to get data from all edit text fields.
+                time = timeEditText.getText().toString();
+                consumed = consumedEditText.getText().toString();
+
+                // validating if the text fields are empty or not.
+                if (time.isEmpty() && consumed.isEmpty()) {
+                    Toast.makeText(getContext(), "Please enter all the data..", Toast.LENGTH_SHORT).show();
+                    return;
                 }
-            });
-        return inflater.inflate(R.layout.fragment_stats, container, false);
 
+                // on below line we are calling a method to add new
+                // course to sqlite data and pass all our values to it.
+                db.addNewDrink(time, consumed);
+
+                // after adding the data we are displaying a toast message.
+                Toast.makeText(getContext(), "Course has been added.", Toast.LENGTH_SHORT).show();
+                timeEditText.setText("");
+                consumedEditText.setText("");
+
+                break;
         }
-
+    }
 }
