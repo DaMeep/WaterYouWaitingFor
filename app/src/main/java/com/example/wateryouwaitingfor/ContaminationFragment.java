90ec29f979fe27562d484f7ca7adb9b196aa0844
Ashwina Bangari <ashbangari@gmail.com>
2023-04-27 17:49:19 -0400

    sql update: data is getting input

--- app/src/main/java/com/example/wateryouwaitingfor/ContaminationFragment.java
+++ app/src/main/java/com/example/wateryouwaitingfor/ContaminationFragment.java
@@ -17,6 +17,7 @@ import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
+
 import java.util.ArrayList;
 
 /**
--- app/src/main/java/com/example/wateryouwaitingfor/DBHandler.java
+++ app/src/main/java/com/example/wateryouwaitingfor/DBHandler.java
@@ -6,36 +6,39 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.ArrayList;
 
 public class DBHandler extends SQLiteOpenHelper {
 
         // creating a constant variables for our database.
         // below variable is for our database name.
-        private static final String DB_NAME = "waterconsumedb";
+        private static final String DB_NAME = "WaterConsumed";
 
         // below int is our database version
-        private static final int DB_VERSION = 1;
+        private static final int DB_VERSION = 4;
 
         // below variable is for our table name.
-        private static final String TABLE_NAME = "dailywater";
-        // below variable is for our second table name.
-        private static final String SECONDTABLE_NAME = "overalldata";
+        private static final String TABLE_NAME = "WaterIntake";
 
         // below variable is for our id column.
         private static final String ID_COL = "id";
 
         // below variable is for our time of consumption column
-        private static final String TIME_COL = "time";
+        private static final String TIME_COL = "Time";
 
         // below variable is for our amount consumed column.
-        private static final String AMOUNT_COL = "amount";
+        private static final String AMOUNT_COL = "Amount";
 
-        // below variable is for our date column for table 2.
-        private static final String DATE_COL = "date";
+        // below variable is for our date column.
+        private static final String DATE_COL = "Date";
 
-        // below variable is for our daily total column for Table 2.
-        private static final String DAYTOT_COL = "daily totals";
+        // below variable is for our daily total column.
+        private static final String DAYTOT_COL = "DailyTotals";
+
+
+    public static double dailyTot = 0;
         // creating a constructor for our database handler.
         public DBHandler(Context context) {
             super(context, DB_NAME, null, DB_VERSION);
@@ -49,25 +52,21 @@ public class DBHandler extends SQLiteOpenHelper {
             // setting our column names
             // along with their data types.
             String query = "CREATE TABLE " + TABLE_NAME + " ("
-                    + ID_COL + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-                    + TIME_COL + " TEXT,"
-                    + AMOUNT_COL + " REAL )";
-
-            String querytwo = "CREATE TABLE "+ SECONDTABLE_NAME + " ("
-            + DATE_COL + "TEXT, "
-            + DAYTOT_COL+ "REAL )";
-
-
+                    + ID_COL+ " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    + TIME_COL+ " TEXT,"
+                    + AMOUNT_COL+ " TEXT,"
+                    + DATE_COL+ " TEXT,"
+                    + DAYTOT_COL+ " TEXT)";
 
 
             // at last we are calling a exec sql
             // method to execute above sql query
             db.execSQL(query);
-            db.execSQL(querytwo);
+
         }
 
         // this method is used to add new drinks to our sqlite database.
-        public void addNewDrink(String time, String Consumed) {
+        public void addNewDrink(double Consumed) {
 
             // on below line we are creating a variable for
             // our sqlite database and calling writable method
@@ -80,9 +79,15 @@ public class DBHandler extends SQLiteOpenHelper {
 
             // on below line we are passing all values
             // along with its key and value pair.
-            values.put(TIME_COL, time);
-            values.put(AMOUNT_COL, Consumed);
 
+            //String time = LocalTime.now().toString();
+            //String date = LocalDate.now().toString();
+             dailyTot += Consumed;
+
+            values.put(TIME_COL, LocalTime.now().toString());
+            values.put(AMOUNT_COL, Consumed);
+            values.put(DATE_COL, LocalDate.now().toString());
+            values.put(DAYTOT_COL, dailyTot);
             // after adding all values we are passing
             // content values to our table.
             db.insert(TABLE_NAME, null, values);
@@ -90,35 +95,10 @@ public class DBHandler extends SQLiteOpenHelper {
             // at last we are closing our
             // database after adding database.
 
-            addNewDayTot(time, Consumed);
+            db.close();
         }
 
-        // this method is used to add new daily totals to our sqlite database.
-        public void addNewDayTot(String date, String Totals) {
-
-            // on below line we are creating a variable for
-            // our sqlite database and calling writable method
-            // as we are writing data in our database.
-            SQLiteDatabase db = this.getWritableDatabase();
-
-            // on below line we are creating a
-            // variable for content values.
-            ContentValues values2 = new ContentValues();
-
-            // on below line we are passing all values
-            // along with its key and value pair.
-            values2.put(DATE_COL, date);
-            values2.put(DAYTOT_COL, Totals);
-
-            // after adding all values we are passing
-            // content values to our table.
-          //  db.insert(SECONDTABLE_NAME, null, values2);
-
-
-            // at last we are closing our
-            // database after adding database.
-
-        }
+//        }
         @Override
         public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
             // this method is called to check if the table exists already.
@@ -147,14 +127,15 @@ public class DBHandler extends SQLiteOpenHelper {
                 do {
                     int timeind = cursor.getColumnIndex(TIME_COL);
                     int amtConsumind = cursor.getColumnIndex(AMOUNT_COL);
-                   // int dateind = cursor.getColumnIndex(DATE_COL);
+                    int dateind = cursor.getColumnIndex(DATE_COL);
+                    int totind = cursor.getColumnIndex(DAYTOT_COL);
                     // on below line we are adding the data from
                     // cursor to our array list.
                     courseModalArrayList.add(new drinkListHandler(
                             cursor.getString(timeind),
-                            cursor.getDouble(amtConsumind)));
-                            //cursor.getString(dateind)));
-                           // cursor.getDouble(3)));
+                            cursor.getDouble(amtConsumind),
+                            cursor.getString(dateind),
+                           cursor.getDouble(totind)));
                 } while (cursor.moveToNext());
                 // moving our cursor to next.
             }
